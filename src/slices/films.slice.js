9ef6a45d9ab62c951e08051827d6eb6ba3a5ba6f
Михаил Дорошин <demargorn@gmail.com>
2025-02-26 22:28:21 +0300

    clear action and comments added

--- src/slices/films.slice.js
+++ src/slices/films.slice.js
@@ -11,10 +11,11 @@ const filmsSlice = createSlice({
    name: 'films',
    initialState,
    reducers: {
+      // загрузить данные
       add: (state, { payload }) => {
          state.all.push(payload);
       },
-
+      // добавить в избранное
       favorite: (state, { payload }) => {
          const { imdbID } = payload;
          const existing = state.favorite.find((s) => s.imdbID === imdbID);
@@ -27,21 +28,25 @@ const filmsSlice = createSlice({
          state.favorite.push(payload);
          alert('Film added succesfully');
       },
-
+      // удалить из избранного
       remove: (state, { payload }) => {
          const { imdbID } = payload;
          state.favorite = state.favorite.filter((s) => s.imdbID !== imdbID);
       },
-
+      // индикатор загрузки
       loading: (state, { payload }) => {
          return { ...state, loading: payload };
       },
-
+      // индикатор ошибки
       isError: (state, { payload }) => {
          return { ...state, error: payload };
+      },
+      // очистка хранилища
+      clear: (state) => {
+         state.all = [];
       },
    },
 });
 
-export const { add, favorite, remove, isError, loading } = filmsSlice.actions;
+export const { add, favorite, remove, isError, loading, clear } = filmsSlice.actions;
 export default filmsSlice.reducer;

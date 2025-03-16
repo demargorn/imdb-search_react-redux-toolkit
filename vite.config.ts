c1e788025a141b342455a6906b7c7048ba624c3a
Михаил Дорошин <demargorn@gmail.com>
2025-03-17 00:13:32 +0300

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'url';
 // https://vite.dev/config/
 export default defineConfig({
    plugins: [react()],
+   base: '/imdb-search_react-redux-toolkit/',
    resolve: {
       alias: {
          '@': fileURLToPath(new URL('./src', import.meta.url)),
